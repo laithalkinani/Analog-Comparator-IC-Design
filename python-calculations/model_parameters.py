47e5f17289fa8158d361096bc80e938a36ec326f
Anthrax Waffle <144612314+abd-alaj@users.noreply.github.com>
2026-03-02 15:54:17 -0500

    bug fixes in parameters.py file

--- python-calculations/model_parameters.py
+++ python-calculations/model_parameters.py
@@ -22,8 +22,13 @@ class MOSModel:
         self.CGDO = CGDO
         self.JS = JS
         self.COX = 3.9 * 8.854187817e-12 / TOX
+        self.k = 0.5 * self.UO * self.COX
 
-    k = 0.5 * self.U0 * self.COX
+    def get_k(self) -> float: 
+        return self.k
+    
+    def get_COX(self) -> float: 
+        return self.COX
 
     def cal_vth(self, VSB: float = 0.0) -> float:
         return self.VTO + self.GAMMA * (np.sqrt(np.abs(VSB + self.PHI)) - np.sqrt(np.abs(self.PHI)))
@@ -32,14 +37,14 @@ class MOSModel:
 # initialize objects
 
 # NMOS model
-nmos_lvl1 = MOSModel(
+nmos= MOSModel(
     LEVEL=1, VTO=0.7, GAMMA=0.45, PHI=0.9, NSUB=9e+14, LD=0.08e-6, 
     UO=350, LAMBDA=0.1, TOX=9e-9, PB=0.9, CJ=0.56e-3, 
     CJSW=0.35e-11, MJ=0.45, MJSW=0.2, CGDO=0.4e-9, JS=1.0e-8
 )
 
 # PMOS model
-pmos_lvl1 = MOSModel(
+pmos= MOSModel(
     LEVEL=1, VTO=-0.8, GAMMA=0.4, PHI=0.8, NSUB=5e+14, LD=0.09e-6, 
     UO=100, LAMBDA=0.2, TOX=9e-9, PB=0.9, CJ=0.94e-3, 
     CJSW=0.32e-11, MJ=0.5, MJSW=0.3, CGDO=0.3e-9, JS=0.5e-8
