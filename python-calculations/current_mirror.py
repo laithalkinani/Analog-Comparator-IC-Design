d35049a5300f3fe10c97c269e128b215b2a3ef72
Anthrax Waffle <144612314+abd-alaj@users.noreply.github.com>
2026-03-04 10:41:50 -0500

    adjusted parameters to be in m, A, V where applicable. plus bug fixes in current mirror, added ISS skeleton function

--- python-calculations/current_mirror.py
+++ python-calculations/current_mirror.py
@@ -1,16 +1,19 @@
 import numpy as np 
-from model_parameters import MOSModel, nmos_lvl1, pmos_lvl1
+from model_parameters import model, nmos, pmos
 
 
 # current mirror in relation to V_DD
 # these use NMOS transistors
 # the assumption is that W and L of all M in current mirror are the same 
-def CM_minWLForSaturation(model: MOSModel, VGS, ID, VSB: float = 0) -> float: 
+def CM_minWLForSaturation(model: model, VGS, ID, VSB: float = 0) -> float: 
     
-    Vov = VGS - model.cal_vth(model, VSB)
+    Vov = VGS - model.cal_vth(VSB)
 
-    if Vov >= 0: 
+    if Vov <= 0: 
         return float('inf') # impossible for current to conduct in saturation
     
-    return ID / (MOSmodel.k) * (Vov ** 2) * (1 + MOSmodel.LAMBDA * VGS) # this is the min ratio needed
+    return (2 * ID) / (model.get_k() * (Vov ** 2) * (1 + model.LAMBDA * VGS)) # this is the min ratio needed
 
+def cal_ISS(model: model, VGS, ID, VSB: float = 0) -> float: 
+    
+    return 
--- python-calculations/model_parameters.py
+++ python-calculations/model_parameters.py
@@ -2,16 +2,16 @@ import numpy as np
 
 # create a class of MOS with their respective attributes 
 # lowk overkill but i don't know how to do this, i was taught only C/C++
-class MOSModel:
+class model:
     def __init__(self, LEVEL, VTO, GAMMA, PHI, NSUB, LD, UO, LAMBDA, 
                  TOX, PB, CJ, CJSW, MJ, MJSW, CGDO, JS):
         self.LEVEL = LEVEL
         self.VTO = VTO
         self.GAMMA = GAMMA
         self.PHI = PHI
-        self.NSUB = NSUB
+        self.NSUB = NSUB * 10e-6
         self.LD = LD
-        self.UO = UO
+        self.UO = UO * 1e-4
         self.LAMBDA = LAMBDA
         self.TOX = TOX
         self.PB = PB
@@ -37,14 +37,14 @@ class MOSModel:
 # initialize objects
 
 # NMOS model
-nmos= MOSModel(
+nmos= model(
     LEVEL=1, VTO=0.7, GAMMA=0.45, PHI=0.9, NSUB=9e+14, LD=0.08e-6, 
     UO=350, LAMBDA=0.1, TOX=9e-9, PB=0.9, CJ=0.56e-3, 
     CJSW=0.35e-11, MJ=0.45, MJSW=0.2, CGDO=0.4e-9, JS=1.0e-8
 )
 
 # PMOS model
-pmos= MOSModel(
+pmos= model(
     LEVEL=1, VTO=-0.8, GAMMA=0.4, PHI=0.8, NSUB=5e+14, LD=0.09e-6, 
     UO=100, LAMBDA=0.2, TOX=9e-9, PB=0.9, CJ=0.94e-3, 
     CJSW=0.32e-11, MJ=0.5, MJSW=0.3, CGDO=0.3e-9, JS=0.5e-8
